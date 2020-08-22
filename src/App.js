0b0118ab6fa748e4dd6ea8ada5236861125467fd
Huu Nhan <huunhankirigamer@gmail.com>
2020-08-22 16:44:16 +0700

    Using react router to create home, about, contact page and add demo image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,60 @@
 import React from 'react';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import logo from './img/The_Book_Depository.svg';
+
+export default function App() {
   return (
-    <div className="App">
-      Company Introduction
-    </div>
+    <Router>
+      <div className="App">
+        <div className="Header">
+          <div className="Nav">
+            
+            <ul>
+              <li>
+                <Link to="/">
+                  <img src={logo} alt="logo"/>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        
+        <div className="Body">
+          <Switch>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
-
-export default App;
